70c1d578d69eff759a530ac8d03f9315ba07d6a3
linus <lagbleze@st.ug.edu.gh>
2025-07-12 12:32:22 +0200

    feat: add package version logging

--- src/review_classifier/__init__.py
+++ src/review_classifier/__init__.py
@@ -1,3 +1,13 @@
 # read version from installed package
 from importlib.metadata import version
-__version__ = version("review_classifier")
+import logging
+
+package = __name__
+__version__ = version(package)
+
+
+logging.basicConfig(level=logging.DEBUG, 
+                    format="%(asctime)s - %(levelname)s - %(message)s"
+                    )
+logger = logging.getLogger(__name__)
+logger.info(f"{package} version {__version__}")
