803d4de2ec9eb4d9df21916d8d58624832ab27fd
linus <lagbleze@st.ug.edu.gh>
2025-07-12 12:37:49 +0200

    feat: import logger

--- src/review_classifier/model/trainer.py
+++ src/review_classifier/model/trainer.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from tqdm import tqdm_notebook, tqdm
 import argparse
 from typing import Literal
+import json
 
 from review_classifier.utils.helpers import (load_glove_from_file, 
                                             set_seed_everywhere, handle_dirs,
@@ -16,26 +17,15 @@ from review_classifier.model.classifier import ReviewClassifier
 from review_classifier.preprocess.vectorizer import ReviewVectorizer
 from review_classifier.utils.embedding_matrix import EmbeddingMatrixMaker
 from review_classifier.utils.helpers import predict_category, get_samples
+from review_classifier import logger
 import torch
-import logging
 import os
-
-logging.basicConfig(level=logging.DEBUG, 
-                    format="%(asctime)s - %(levelname)s - %(message)s"
-                    )
-logger = logging.getLogger(__name__)
-
-
-#%%
-
-
 
 def train(model, num_epochs: int,
           loss_func, train_state: dict, dataset, optimizer,
-          scheduler, #generate_batches_func=generate_batches,
+          scheduler,
           update_train_state_func=update_train_state,
-          evaluate_func=compute_accuracy, batch_size=32, 
-          #device='cuda',
+          evaluate_func=compute_accuracy, batch_size=32,
           early_stopping_criteria=5,
           tqdm_in_cli=True,
           ):
@@ -73,51 +63,9 @@ def train(model, num_epochs: int,
         for epoch_index in range(num_epochs):
             train_state['epoch_index'] = epoch_index
 
-            # Iterate over training dataset
-
-            # setup: batch generator, set loss and acc to 0, set train mode on
-
-            # dataset.set_split('train')
-            # ##############################         ###################
-            # batch_generator = generate_batches_func(dataset, 
-            #                                         batch_size=batch_size, 
-            #                                         device=device
-            #                                         )
             running_loss = 0.0
             running_acc = 0.0
-            #model.train()
 
-            #for batch_index, batch_dict in enumerate(batch_generator):
-                # the training routine is these 5 steps:
-
-                # --------------------------------------
-                # step 1. zero the gradients
-                # optimizer.zero_grad()
-
-                # # step 2. compute the output
-                # y_pred = model(batch_dict['x_data'])
-
-                # # step 3. compute the loss
-                # loss = loss_func(y_pred, batch_dict['y_target'])
-                # loss_t = loss.item()
-                # running_loss += (loss_t - running_loss) / (batch_index + 1)
-
-                # # step 4. use loss to produce gradients
-                # loss.backward()
-
-                # # step 5. use optimizer to take gradient step
-                # optimizer.step()
-                # # -----------------------------------------
-                # # compute the accuracy
-                # acc_t = evaluate_func(y_pred, batch_dict['y_target'])
-                # running_acc += (acc_t - running_acc) / (batch_index + 1)
-
-                # # update bar
-                # train_bar.set_postfix(loss=running_loss, 
-                #                       acc=running_acc,
-                #                       epoch=epoch_index
-                #                       )
-                # train_bar.update()
             model, running_loss, running_acc, mode_bar = compute_model_performance(model=model,
                                         mode="train",
                                         dataset=dataset,
@@ -133,36 +81,9 @@ def train(model, num_epochs: int,
             train_state['train_loss'].append(running_loss)
             train_state['train_acc'].append(running_acc)
 
-            # Iterate over val dataset
-
-            # setup: batch generator, set loss and acc to 0; set eval mode on
-            # dataset.set_split('val')
-            # ##############################         ###################
-            # batch_generator = generate_batches_func(dataset, 
-            #                                         batch_size=batch_size, 
-            #                                         device=device
-            #                                         )
             running_loss = 0.
             running_acc = 0.
-            #model.eval()
 
-            #for batch_index, batch_dict in enumerate(batch_generator):
-
-                # # compute the output
-                # y_pred =  model(batch_dict['x_data'])
-
-                # # step 3. compute the loss
-                # loss = loss_func(y_pred, batch_dict['y_target'])
-                # loss_t = loss.item()
-                # running_loss += (loss_t - running_loss) / (batch_index + 1)
-
-                # # compute the accuracy
-                # acc_t = evaluate_func(y_pred, batch_dict['y_target'])
-                # running_acc += (acc_t - running_acc) / (batch_index + 1)
-                # val_bar.set_postfix(loss=running_loss, acc=running_acc, 
-                #                     epoch=epoch_index
-                #                     )
-                # val_bar.update()
             model, running_loss, running_acc, mode_bar = compute_model_performance(model=model,
                                         mode="val",
                                         dataset=dataset,
@@ -192,9 +113,7 @@ def train(model, num_epochs: int,
             epoch_bar.update()
         return model, train_state
     except KeyboardInterrupt:
-        print("Exiting loop")
-
-
+        logger.info("User interrupt... Stopping training.")
 
 
 def compute_model_performance(model, mode: Literal["train", "val", "test"],
@@ -205,8 +124,6 @@ def compute_model_performance(model, mode: Literal["train", "val", "test"],
                               optimizer: optim.Optimizer = None,
                               mode_bar=None, 
                               device="cuda",
-                              #epoch_bar=None,
-                              #mode_state: dict = None,
                               ):
                                   
     if mode == "train":
@@ -246,71 +163,13 @@ def compute_model_performance(model, mode: Literal["train", "val", "test"],
         if mode == "train":
             loss.backward()
             optimizer.step()
-            
-            # if mode_bar is not None:
-            #     mode_bar.set_postfix(loss=running_loss, acc=running_acc, 
-            #                         epoch=epoch_index
-            #                         )
-            #     mode_bar.update()
-        #elif mode == "val":
         if mode_bar is not None:
             mode_bar.set_postfix(loss=running_loss, acc=running_acc, 
                                 epoch=epoch_index
                                 )
             mode_bar.update()
     return model, running_loss, running_acc, mode_bar
-    # if mode == "train":
-    #     mode_state['train_loss'].append(running_loss)
-    #     mode_state['train_acc'].append(running_acc)
-    # elif mode == "val":
-    #     mode_state['val_loss'].append(running_loss)
-    #     mode_state['val_acc'].append(running_acc)
-        
-    #     mode_state = update_train_state_func(early_stopping_criteria,
-    #                                           model=model,
-    #                                           train_state=mode_state
-    #                                           )
-    #     scheduler.step(mode_state['val_loss'][-1])
-    #     if mode_state['stop_early']:
-    #         print("Early stopping triggered")
-    #         #return train_state
-            
-    # elif mode == "test":
-    #     mode_state['test_loss'] = running_loss
-    #     mode_state['test_acc'] = running_acc
-    # return mode_state, mode_bar  
 
-#%% Inference
-# Preprocess the reviews
-
-
-#%%
-# def predict_category(title, classifier, vectorizer, max_length):
-#     """Predicts a news category for a new title
-    
-#     Args:
-#         title (str): a raw title string
-#         classifier (NewsVectorizer): an instanve of the trained classifier
-#         vectorizer (NewsVectorizer): the corresponding vectorizer
-#         max_length (int): the max sequence length
-#             CNN are sensitive to the input data tensor size, 
-#             This ensures to keep it the same size as the training data
-#     """
-#     title = preprocess_text(title)
-#     vectorized_title = torch.tensor(vectorizer.vectorize(title, vector_length=max_length))
-#     result = classifier(vectorized_title.unsqueeze(0), apply_softmax=True)
-#     probability_values, indices = result.max(dim=1)
-#     predicted_category = vectorizer.category_vocab.lookup_index(indices.item())
-    
-#     return {'category': predicted_category,
-#             'probability': probability_values.item()
-#             }
-
-
-
-#%%
-#title = input("Enter a news title to classify: ")
-#classifier = classifier.to("cpu")
 
 def model_diagnostics(model, data: dict, vectorizer: ReviewVectorizer, 
                       max_seq_length: int #= dataset._max_seq_length + 1
@@ -328,11 +187,6 @@ def model_diagnostics(model, data: dict, vectorizer: ReviewVectorizer,
             print("\t + Sample: {}".format(sample))
         print("-"*30 + "\n")
     
-
-
-#%%
-#predict_category("I the best product", classifier, vectorizer, dataset._max_seq_length +1)
-
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Train a CNN for product reviews classification")
@@ -382,7 +236,6 @@ def parse_arguments():
                         )
     parser.add_argument("--reload_from_files", action="store_true")
     parser.add_argument("--expand_filepaths_to_save_dir", action="store_true",)
-    #parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--max_seq_length", default=1646, type=int,)
     parser.add_argument("--resume_train", action="store_true",
                         help="Resume training from the last checkpoint. Will use the model_state_file to load the model state and continue training."
@@ -396,8 +249,7 @@ def parse_arguments():
     parser.add_argument("--num_samples_to_diagnose", type=int, default=5,
                         help="Number of samples to diagnose after training. This will print the predictions for a few samples from the test set."
                         )
-    
-    
+        
     return parser.parse_args()
 
 
@@ -416,7 +268,6 @@ def main():
         print("\t{}".format(args.vectorizer_file))
         print("\t{}".format(args.model_state_file))
         
-        # Check CUDA
     if not torch.cuda.is_available():
         cuda = False
     else:
@@ -431,48 +282,39 @@ def main():
     # handle dirs
     handle_dirs(args.save_dir)
 
-
-    #%% Initializations
-    #args.use_glove = True
-
     if args.reload_from_files:
-        # training from a checkpoint
         dataset = ReviewDataset.load_dataset_and_load_vectorizer(args.data_csv,
                                                             args.vectorizer_file
                                                             )
     else:
         # create dataset and vectorizer
         dataset = ReviewDataset.load_dataset_and_make_vectorizer(args.data_csv)
-        # ReviewVectorizer.from_dataframe(review_df=dataset.train_df,
-        #                                 cutoff=0)
+        
         dataset.save_vectorizer(args.vectorizer_file)
     vectorizer = dataset.get_vectorizer()
 
     #%% Use GloVe or randomly initialized embeddings
     if args.use_glove:
+        logger.info(f"Using GloVe embeddings from {args.glove_filepath}")
         words = vectorizer.review_vocab._token_to_idx.keys()
         embedding_matrix = EmbeddingMatrixMaker(glove_filepath=args.glove_filepath,
                             words=words
                             )
         embeddings = embedding_matrix.make_embedding_matrix()
         embedding_matrix.save_embedding()
-        # embeddings = make_embedding_matrix(glove_filepath=args.glove_filepath,
-        #                                    words=words)
         print("Using pre-trained embeddings")
     else:
-        print("Not using pre-trained embeddings")
+        logger.info("Using randomly initialized embeddings")
         embeddings = None
-        
-    
     classifier = ReviewClassifier(embedding_size=args.embedding_size,
-                                num_embeddings=len(vectorizer.review_vocab),
-                                num_channels=args.num_channels,
-                                hidden_dim=args.hidden_dim,
-                                num_classes=len(vectorizer.category_vocab),
-                                dropout_p=args.dropout_p,
-                                pretrained_embeddings=embeddings,
-                                padding_idx=0, device=device
-                                )
+                                  num_embeddings=len(vectorizer.review_vocab),
+                                  num_channels=args.num_channels,
+                                  hidden_dim=args.hidden_dim,
+                                  num_classes=len(vectorizer.category_vocab),
+                                  dropout_p=args.dropout_p,
+                                  pretrained_embeddings=embeddings,
+                                  padding_idx=0, device=device
+                                  )
     if args.resume_train:
         logger.info(f"Resuming training from {args.model_state_file}")
         classifier.load_state_dict(torch.load(args.model_state_file))      
@@ -512,31 +354,29 @@ def main():
         model_diagnostics(model=model, data=data, vectorizer=vectorizer, 
                         max_seq_length=dataset._max_seq_length + 2
                         ) 
+    
+    
+    logger.info("Evaluating model on the test set")
+    model, running_loss, running_acc, _ = compute_model_performance(model=classifier,
+                                                                    mode="test",
+                                                                    dataset=dataset,
+                                                                    loss_func=loss_func,
+                                                                    evaluate_func=compute_accuracy,
+                                                                    batch_size=args.batch_size,
+                                                                    epoch_index=None,
+                                                                    mode_bar=None
+                                                                    )
+
+    train_state['test_loss'] = running_loss
+    train_state['test_acc'] = running_acc
         
-    
-    
-    
-    
-    
-    
-    
-    
-    # epoch_bar = tqdm_notebook(desc='training routine', 
-    #                           total=args.num_epochs,
-    #                           position=0)
+    logger.info(f"Test loss: {train_state['test_loss']}")
+    logger.info(f"Test Accuracy: {train_state['test_acc']}")
 
-    #dataset.set_split('train')
-    # train_bar = tqdm_notebook(desc='split=train',
-    #                           total=dataset.get_num_batches(args.batch_size), 
-    #                           position=1, 
-    #                           leave=True
-    #                           )
-    #dataset.set_split('val')
-    # val_bar = tqdm_notebook(desc='split=val',
-    #                         total=dataset.get_num_batches(args.batch_size), 
-    #                         position=1, 
-    #                         leave=True)
+    with open("train_state.json", "w") as f:
+        logger.info(f"Saving train state to {f.name}")
+        json.dump(train_state, f, indent=4)
+        
+    logger.info(f"Training complete. Model state saved to {args.model_state_file}")
+    logger.info(f"Train state saved to train_state.json")
 
-
-
-# %%
